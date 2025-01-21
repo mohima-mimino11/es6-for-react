bb2847f86b89ca80e28627eeb1d73f9b0b1d632d
mohima-mimino11 <mohimahoque22@gmail.com>
2025-01-21 20:26:05 +0600

    exercise added

---
+++ exercise.js
@@ -0,0 +1,24 @@
+const returnNine = () => 9;
+
+// console.log(returnNine());
+
+const multiplyTwelve = (x) => x * 12;
+
+// console.log(multiplyTwelve(3));
+
+const twoParameters = (x, y) => (x + y) / 4;
+
+// console.log(twoParameters(4,4));
+
+const multipleLine = (a, b) => {
+  const addFive1 = a + 5;
+  const addFive2 = b + 5;
+  const result = addFive1 * addFive2;
+  return result;
+}
+
+console.log(multipleLine(5, 6));
+
+
+
+
